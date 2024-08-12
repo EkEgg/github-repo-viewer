5329fcc1384f1f3bb1de08c57412a3a2723a9842
Erick Gaiote <45886797+EkEgg@users.noreply.github.com>
2024-08-11 22:18:12 -0300

    Added comment instructing how to add a GitHub access token

--- src/wrappers/octokit.ts
+++ src/wrappers/octokit.ts
@@ -2,4 +2,17 @@ import { Octokit } from "@octokit/core";
 
 const octokit = new Octokit();
 
+/*
+
+// To make requests using a GitHub access token:
+
+const octokit = new Octokit({
+    auth: YOUR_TOKEN
+});
+
+// You can generate a new access token here:
+// https://github.com/settings/tokens/new
+
+*/
+
 export default octokit;
